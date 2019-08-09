9e4ad8344601ea0b12c68e2f7d558e1c7c3824f1
yilunhu <yilunhu31@gmail.com>
2019-08-09 10:50:23 -0400

    Repostirory  Pattern from youtube video Check pls! Version 1.0

---
+++ RevHousingAPI/IRepositories/IRoomRepository.cs
@@ -0,0 +1,13 @@
+﻿using RHEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RevHousingAPI.Repositories
+{
+    public interface IRoomRepository: IRepository<Room>
+    {
+        Room GetRoomWithLocation(int id);
+    }
+}
---
+++ RevHousingAPI/Repositories/RoomRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RHEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RevHousingAPI.Repositories
+{
+    public class RoomRepository : Repository<Room>, IRoomRepository
+    {
+        public RoomRepository(ApplicationDBContext context) : base(context)
+        {
+        }
+        public Room GetRoomWithLocation(int id)
+        {
+            return ApplicationDBContext.Room.Include(a => a.LocationID).SingleOrDefault(a => a.LocationID == id);
+        }
+        public ApplicationDBContext ApplicationDBContext
+        {
+            get { return Context as ApplicationDBContext; }
+        }
+    }
+}
